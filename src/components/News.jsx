e204ef8ff0207f7cc108acb5aa20ba6b462fe5d0
Supriya Chanda <suplife24@gmail.com>
2023-06-06 16:46:18 +0530

    news updated

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BsCalendar2Date } from 'react-icons/bs'
+import { TfiAnnouncement } from 'react-icons/tfi'
 
 const news = [
   {
@@ -16,6 +17,13 @@ const news = [
   },
 ]
 
+const events=[
+  {
+    name:"Apply for Android Development",
+  },
+  
+]
+
 export const News = () => {
 
   return (
@@ -29,13 +37,11 @@ export const News = () => {
                 news.map((item) => {
                   return (
                     <li className='news-li text-[#676767] py-[0.5%] my-[1%] border-b-2 border-[#67b0d1]'>
-                      <span className=' text-[#67b0d1] mr-[2%] flex items-center'>
+                      <span className=' text-[#67b0d1] mr-[2%] flex items-center' >
                       <BsCalendar2Date className='mem-icon mr-1' size={"1rem"} />
                         {item.year}
                       </span>
                       {item.name}
-
-
                     </li>
                   )
                 })
@@ -43,9 +49,25 @@ export const News = () => {
             </ul>
           </div>
         </div>
-        <div className='bg-gray-200 border-2 border-gray-400 news-right w-1/4 flex justify-center '>
-          <div className=''>
-              Apply for Android Development
+        <div className='bg-gray-200  news-right  w-1/4 flex justify-center '>
+        <div className='py-[2%] mx-[2%] border-[1%] '>
+            <ul className=' text-[120%] px-[2%]  list-inside w-full ' >
+              {
+                events.map((item) => {
+                  return (
+                    <li className='news-li text-[#676767]   flex py-[0.5%] my-[1%] '>
+                      <span className=' text-gray-400 mr-[2%]'>
+                      <TfiAnnouncement className='events mem-icon mr-2 mt-2 ' size={"1rem"} />
+                      </span>
+                      <span>
+
+                      {item.name}
+                      </span>
+                      </li>
+                  )
+                })
+              }
+            </ul>
           </div>
         </div>
       </div>
